2407799acc8aed6426e53fd3f650e8621add7dd8
Ryan Graff <ryan@StellarOmega.local>
2017-02-07 23:37:24 -0500

    chore: Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Reincarnate
+
+## Build/Run Demo
+
+1. Create An App: `rdx app`
+1. Compile: `rdx compile` or `rdx compile -a src/index.html`
+1. Serve: `rdx serve` or `rdx serve -a src/index.html`
---
+++ src/ReincarnateDemo.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class ReincarnateDemo extends Component {
+  static propTypes = {};
+  
+  constructor() {
+    super();
+  }
+
+  state = {};
+
+  render() {
+    return (
+      <h1>
+        ReincarnateDemo
+      </h1>
+    );
+  }
+}
